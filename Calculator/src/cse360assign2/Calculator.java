28ca5455310609b81ffdbbd29612cff1bbaf943e
basamalasaly <basamalasaly@localhost>
2019-02-16 17:03:18 -0700

    Version 3: Complete calculator with return string (Tested with file CaclulatorTest.java)

--- Calculator/src/cse360assign2/Calculator.java
+++ Calculator/src/cse360assign2/Calculator.java
@@ -1,13 +1,19 @@
+//Basam Alasaly
+//CSE 360 - 30399
+//02-16-2019
+//Assignment 2 - Calculator File
+
 package cse360assign2;
 
 public class Calculator
 {
-
-	private int total;
+	private static int total;
+	private static String totalReturn = new String();
 	
 	public Calculator ()
 	{
 		total = 0;  // not needed - included for clarity
+		totalReturn = "0 ";
 	}
 	
 	public int getTotal ()
@@ -18,16 +24,19 @@ public class Calculator
 	public void add (int value)
 	{
 		total += value;
+		totalReturn += ("+ " + value + " ");
 	}
 	
 	public void subtract (int value)
 	{
 		total -= value;
+		totalReturn += ("- " + value + " ");
 	}
 	
 	public void multiply (int value)
 	{
 		total *= value;
+		totalReturn += ("* " + value + " ");
 	}
 	
 	public void divide (int value) 
@@ -35,15 +44,17 @@ public class Calculator
 		if(value != 0)
 		{
 			total /= value;
+			totalReturn += ("/ " + value + " ");
 		}
 		else
 		{
 			total = 0;
+			totalReturn += ("/ " + value + " ");
 		}
 	}
 	
 	public String getHistory ()
 	{
-		return "";
+		return(totalReturn);
 	}
 }
